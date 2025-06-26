f80c17d0bd46aca62257034db8d8aaa51b66b940
Pablo Bueno <pablobuenodearaujo@gmail.com>
2025-06-26 19:05:26 -0400

    Delete src directory

--- src/file-compressor/file-compressor.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FileCompressorService } from './file-compressor.service';
-import { FileCompressorController } from './file-compressor.controller';
-
-@Module({
-  providers: [FileCompressorService],
-  controllers: [FileCompressorController],
-})
-export class FileCompressorModule {}
--- src/merger-pdf/merger-pdf.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MergerPdfController } from './merger-pdf.controller';
-import { MergerPdfService } from './merger-pdf.service';
-
-@Module({
-  controllers: [MergerPdfController],
-  providers: [MergerPdfService],
-})
-export class MergerPdfModule {}
