e57860be40129b265122d8e88af1672a8d631408
Pablo Bueno <pablobuenodearaujo@gmail.com>
2025-07-03 20:42:20 -0400

    Delete src directory

--- src/pdf-image-converter/pdf-image.module.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./pdf-image.controller');
-
-router.post('/pdf-to-images', express.json(), controller.pdfToImages);
-router.post('/images-to-pdf', express.json(), controller.imagesToPdf);
-
-module.exports = router;
